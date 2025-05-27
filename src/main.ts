e0d58301c65888c9062749afb199bc218d8e6872
rinuy <rinuyatim@gmail.com>
2025-05-27 10:38:39 +0200

    feature/hw-vuetify-integration: streamlining setup

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+import './styles/main.scss' // Optional global styles
+
+const app = createApp(App)
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,7 +2,6 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
 import '@mdi/font/css/materialdesignicons.css'
 
 export default createVuetify({
@@ -10,5 +9,20 @@ export default createVuetify({
   directives,
   icons: {
     defaultSet: 'mdi',
+  },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+        },
+      },
+    },
   },
 })
