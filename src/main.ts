6f75432ac7e5125f7d16ca828b992eeb5ff1fb1e
rinuy <rinuyatim@gmail.com>
2025-05-27 10:13:14 +0200

    feature/hw-vuetify-integration: separate styles

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-// Vuetify core
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-// Material Design Icons (optional)
-import '@mdi/font/css/materialdesignicons.css'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+import vuetify from './plugins/vuetify'
 
 createApp(App)
   .use(router)
