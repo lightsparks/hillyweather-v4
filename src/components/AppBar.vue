0abfdd89f15e7b1e1398337552dca72a6f74f0a1
rinuy <rinuyatim@gmail.com>
2025-06-02 20:24:49 +0200

    remove slash to let Vite's base doe the work

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -5,7 +5,7 @@
       <div class="d-flex align-center">
         <div class="app-bar-icon-wrapper">
           <v-img
-            src="/app-bar-icon.png"
+            src="app-bar-icon.png"
             alt="App Icon"
             class="app-bar-icon"
             cover
